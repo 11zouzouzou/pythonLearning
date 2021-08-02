8fa17d1b2ed89ff84453450a870302951c6a14b5
邹静磊 <zoujl@shinemo.com>
2021-08-02 19:19:44 +0800

    json read and write

---
+++ src/dataProcessing/JSONProcessing.py
@@ -0,0 +1,20 @@
+import json
+# parse
+jsonstring = '{"name": "erik", "age": 38, "married": true}'
+data = json.loads(jsonstring)
+print('dict',data,type(data))
+#encoding
+encodingData = {'name': 'erik', 'age': 38, 'married': True}
+print('string',json.dumps(encodingData,indent=2))# indent 控制缩进
+
+# echo "{ \"name\": \"Monty\", \"age\": 45 }" python3 -m json.tool
+# 写入json
+writeData = {'name': 'zou', 'age': 18 }
+with open('sources/writeData.json','w') as json_file:
+  json.dump(writeData,json_file,indent=2)
+  print(writeData)
+# 读取json
+data={}
+with open('sources/map.json') as json_file:
+  data = json.load(json_file)
+  print(data)
